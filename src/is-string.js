c2f7bc050ac56d9aea33581dd29d30535abf769b
Haixing Hu <starfish.hu@gmail.com>
2026-02-28 10:32:27 +0800

    Optimize isString function with typeof pre-validation for better performance

--- src/is-string.js
+++ src/is-string.js
@@ -20,6 +20,12 @@
  * @see <a href="https://github.com/sindresorhus/is/tree/main?tab=readme-ov-file#why-not-just-use-instanceof-instead-of-this-package">Why not just use instanceof instead of this package?</a>
  */
 function isString(value) {
+  if (typeof value === 'string') {
+    return true;
+  }
+  if (typeof value !== 'object' || value === null) {
+    return false;
+  }
   return Object.prototype.toString.call(value) === '[object String]';
 }
 
