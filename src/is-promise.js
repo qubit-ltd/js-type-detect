c4edaa91d405762e0ec0d73c35adb623d39cbeb5
Haixing Hu <starfish.hu@gmail.com>
2026-02-28 10:31:29 +0800

    Optimize isPromise function with instanceof fast path detection

--- src/is-promise.js
+++ src/is-promise.js
@@ -17,15 +17,22 @@
  * @author Haixing Hu
  */
 function isPromise(value) {
-  if (typeof Promise === 'undefined' || typeof value !== 'object' || value === null) {
+  if (typeof value !== 'object' || value === null || typeof Promise === 'undefined') {
     return false;
+  }
+  try {
+    if (value instanceof Promise) {
+      return true;
+    }
+  } catch (e) {
+    // ignore error during instanceof check
   }
   // use toString value to cross realms
   if (Object.prototype.toString.call(value) === '[object Promise]') {
     return true;
   }
   try {
-    return ((typeof value.then === 'function') && (typeof value.catch === 'function'));
+    return (typeof value.then === 'function') && (typeof value.catch === 'function');
   } catch (e) {
     return false;
   }
